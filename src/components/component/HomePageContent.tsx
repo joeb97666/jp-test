cc620290016cba6b0600d11bfbfac981d76483fc
David Bonthuis <dab@Davids-MBP.home>
2024-02-06 07:20:33 -0800

    update02

--- src/components/component/HomePageContent.tsx
+++ src/components/component/HomePageContent.tsx
@@ -31,7 +31,7 @@ export function HomeGrass() {
               <div className="flex justify-center space-x-4 lg:justify-start">
                 <Link
                   className="inline-flex h-9 items-center justify-center rounded-md border border-gray-200 border-gray-200 bg-white px-4 py-2 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
-                  href="#"
+                  href="./Contact"
                 >
                   Contact Us
                 </Link>
@@ -56,7 +56,7 @@ export function HomeGrass() {
               <div className="flex justify-center space-x-4 lg:justify-start">
                 <Link
                   className="inline-flex h-9 items-center justify-center rounded-md border border-gray-200 border-gray-200 bg-white px-4 py-2 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
-                  href="Services#"
+                  href="./Services"
                 >
                   Services
                 </Link>
